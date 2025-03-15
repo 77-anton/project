bb9cead18509fa68c44578bcfd9e3d584354280a
77-anton <prkpnkanton@gmail.com>
2025-03-15 14:02:07 +0200

    Update guess the number

--- Guess the number.py
+++ Guess the number.py
@@ -1,26 +1,33 @@
 import random
+import time
+import sys
 
-# Генеруємо випадкове число від 1 до 10
-secret_number = random.randint(1, 10)
 
-print("Вгадай число від 1 до 10!")
+# Анімація
+def long_loading_animation():
+    animation = ["|", "/", "-", "\\"]
+    print("Гра завантажується...", end="", flush=True)
+    for _ in range(20):
+        for symbol in animation:
+            time.sleep(0.2)
+            print(f"\rГра завантажується... {symbol}", end="", flush=True)
+    print("\nГотово!")
 
-while True:
-    try:
-        # Користувач вводить число
-        guess = int(input("Введи своє число: "))
 
-        # Перевіряємо, чи вгадав користувач
-        if guess == secret_number:
-            print("Вітаю! Ти вгадав число!")
-            break
-        elif guess < secret_number:
-            print("Загадане число більше.")
-        else:
-            print("Загадане число менше.")
+# Анімація
+long_loading_animation()
+print("Вітаю в грі 'Вгадай число!'")
 
-    except ValueError:
-        # Обробка випадку, якщо користувач ввів не число
-        print("Будь ласка, введіть ціле число.")
+number = random.randint(1, 1000000)
 
-print("Гра закінчена.")
+guess = 0
+
+while guess != number:
+    guess = int(input("Вгадай число від 1 до 1000000: "))
+
+    if guess < number:
+        print("Загадане число більше. Спробуй ще раз!")
+    elif guess > number:
+        print("Загадане число менше. Спробуй ще раз!")
+    else:
+        print(f"Вітаю! Ти вгадав число {number}!")
